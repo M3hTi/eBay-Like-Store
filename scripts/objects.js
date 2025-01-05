e5e67eef58f71aff934fc7f085a813dfb64a9ac8
M3hTi <mehdinasir502@yahoo.com>
2025-01-05 19:25:07 +0330

    feat: add total property and calculateTotalPrice method to cart object

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,13 +1,17 @@
 const cart = {
     products : [],
+    total : 0,
     addProduct : function (obj) {
         this.products.push(obj)
+    },
+    calculateTotalPrice : function() {
+        this.total = 0
+        this.products.forEach(product => {
+            this.total += (product.price * Number(product.quantity))
+        })
+        return this.total / 100
     }
 }
-
-
-
-
 
 class Product {
     constructor(image, title , price , quantity , id) {
